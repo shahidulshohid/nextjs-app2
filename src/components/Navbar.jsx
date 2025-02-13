c61746b6335488ba5058664c42c6443f2cdc8b82
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-13 09:17:38 +0600

    nextjs app2 first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavBar = () => {
+  const pathName = usePathname();
+  console.log(pathName, pathName.includes("dashboard"));
+  if (!pathName.includes("dashboard")) {
+    return (
+      <div>
+        <nav className="flex justify-center">
+          <ul className="flex justify-between w-1/2">
+            <Link href="/">
+              <li>Home</li>
+            </Link>
+            <Link href="/services">
+              <li>Services</li>
+            </Link>
+            <Link href="/about">
+              <li>About Us</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+};
+
+export default NavBar;
